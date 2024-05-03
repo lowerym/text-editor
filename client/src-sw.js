a17c1af65a08087e3f979ad6edc8d00831632c88
Matthew Lowery <matthewdlowery@msn.com>
2024-05-02 21:27:50 -0500

    Built out src-sw, database, install, webpack, and package files

--- client/src-sw.js
+++ client/src-sw.js
@@ -26,5 +26,22 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Implements asset cache
+registerRoute(
+  // This defines the callback function that filters the requests we want cached, namely JS and CSS files
+    ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+    new StaleWhileRevalidate({
+      // This is what we'll be naming the cache
+      cacheName: 'asset-cache',
+      plugins: [
+        // This plugin lets us cache responses using headers with a maximum-age of 30 days
+        new CacheableResponsePlugin({
+          statuses: [0, 200],
+        }),
+        new ExpirationPlugin({
+          maxEntries: 120,
+          maxAgeSeconds: 30 * 24 * 60 * 60, //30 Days
+        })
+      ],
+    })
+  );
--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,45 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('PUT to the Database');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  // Creates a connection to the database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Creates a new transaction and specifies the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readwrite');
+
+  // Opens up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Uses the .add() method on the store and passes in the content.
+  const request = store.put({ id: 1, value: content });
+
+  // Gets confirmation of the request.
+  const result = await request;
+  console.log('Data saved to the database', result);
+};
+
+export const getDb = async () => {
+  console.error('GET from the Database');
+
+  // Creates a connection to the database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Creates a new transaction and specifies the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  // Opens up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Uses the .add() method on the store and passes in the content.
+  const request = store.getAll();
+
+  // Gets confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result?.value;
+};
 
 initdb();
